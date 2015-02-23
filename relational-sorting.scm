f6f6d3baffb7204e3d81448c18be7ceaa4ef1ba5
William E. Byrd <webyrd@gmail.com>
2015-02-23 12:48:39 -0700

    implemented merge sort

--- relational-sorting.scm
+++ relational-sorting.scm
@@ -191,3 +191,140 @@
 (test "orchid-inserto-1"
   (run* (q) (inserto '(s (s (s z))) '(z (s (s z)) (s (s (s (s z))))) q))
   '((z (s (s z)) (s (s (s z))) (s (s (s (s z)))))))
+
+
+
+;;; merge sort
+;;;
+;;; code and tests adapted from 'http://gauss.ececs.uc.edu/Courses/C511/html/Scheme/mergesrt.html'
+
+(define even-numberso
+  (lambda (l out)
+    (conde
+      ((== '() l)
+       (== '() out))
+      ((fresh (a)
+         (== `(,a) l)
+         (== '() out)))
+      ((fresh (a ad dd res)
+         (== `(,a ,ad . ,dd) l)
+         (== `(,ad . ,res) out)
+         (even-numberso dd res))))))
+
+(define odd-numberso
+  (lambda (l out)
+    (conde
+      ((== '() l)
+       (== '() out))
+      ((fresh (a d)
+         (== `(,a . ,d) l)
+         (conde
+           ((== '() d)
+            (== `(,a) out))
+           ((fresh (ad dd res)
+              (== `(,ad . ,dd) d)
+              (== `(,a . ,res) out)
+              (odd-numberso dd res)))))))))
+
+(define merge-listso
+  (lambda (l1 l2 out)
+    (conde
+      ((== '() l1)
+       (== l2 out))
+      ((fresh (a d)
+         (== `(,a . ,d) l1)
+         (== '() l2)
+         (== l1 out)))
+      ((fresh (a-l1 d-l1 a-l2 d-l2 res)
+         (== `(,a-l1 . ,d-l1) l1)
+         (== `(,a-l2 . ,d-l2) l2)
+         (conde
+           ((== `(,a-l1 . ,res) out)
+            (<=o a-l1 a-l2)
+            (merge-listso d-l1 l2 res))
+           ((=/= a-l1 a-l2)
+            (== `(,a-l2 . ,res) out)
+            (<=o a-l2 a-l1)
+            (merge-listso d-l2 l1 res))))))))
+
+(define merge-sorto
+  (lambda (l out)
+    (conde
+      ((== '() l)
+       (== l out))
+      ((fresh (a d)
+         (== `(,a . ,d) l)
+         (conde
+           ((== '() d)
+            (== l out))
+           ((fresh (ad dd e o res-e res-o)
+              (== `(,ad . ,dd) d)
+              (odd-numberso l e)
+              (even-numberso l o)
+              (merge-sorto e res-e)
+              (merge-sorto o res-o)
+              (merge-listso res-e res-o out)))))))))
+
+(test "even-numberso-1"
+  (run* (q) (even-numberso '(2 7 6 5 4 5 6 7 4) q))
+  '((7 5 5 7)))
+
+(test "odd-numberso-1"
+  (run* (q) (odd-numberso '(2 7 6 5 4 5 6 7 4) q))
+  '((2 6 4 6 4)))
+
+(test "merge-listso-0"
+  (run* (q) (merge-listso '() '() q))
+  '(()))
+
+(test "merge-listso-1"
+  (run* (q) (merge-listso '(z) '() q))
+  '((z)))
+
+(test "merge-listso-2"
+  (run* (q) (merge-listso '() '(z) q))
+  '((z)))
+
+(test "merge-listso-3"
+  (run* (q) (merge-listso '(z) '(z) q))
+  '((z z)))
+
+(test "merge-listso-4"
+  (run* (q) (merge-listso '((s z)) '(z) q))
+  '((z (s z))))
+
+(test "merge-listso-5"
+  (run* (q) (merge-listso '((s z)) '(z (s (s z))) q))
+  '((z (s z) (s (s z)))))
+
+(test "merge-listso-6"
+  (run* (q) (merge-listso '(z (s z) (s (s z))) '((s (s (s z))) (s (s (s (s z)))) (s (s (s (s (s z)))))) q))
+  '((z (s z) (s (s z)) (s (s (s z))) (s (s (s (s z)))) (s (s (s (s (s z))))))))
+
+(test "merge-sorto-1"
+  (run* (q) (merge-sorto '(z (s z) (s (s z)) (s (s (s z))) (s (s (s (s z)))) (s (s (s (s (s z)))))) q))
+  '((z (s z) (s (s z)) (s (s (s z))) (s (s (s (s z)))) (s (s (s (s (s z))))))))
+
+(test "merge-sorto-2"
+  (run* (q) (merge-sorto '((s z) (s (s (s z))) (s (s (s (s (s z))))) z (s (s (s (s z)))) (s (s z))) q))
+  '((z (s z) (s (s z)) (s (s (s z))) (s (s (s (s z)))) (s (s (s (s (s z))))))))
+
+;;; run 7 diverges
+(test "merge-sorto-3"
+  (run 6 (q) (merge-sorto q '(z (s z) (s (s z)))))
+  '((z (s (s z)) (s z))
+    (z (s z) (s (s z)))
+    ((s z) (s (s z)) z)
+    ((s (s z)) (s z) z)
+    ((s z) z (s (s z)))
+    ((s (s z)) z (s z))))
+
+;;; run 7 diverges
+(test "merge-sorto-4"
+  (run 6 (n m p) (merge-sorto `(,n ,m ,p) '(z (s z) (s (s z)))))
+  '((z (s (s z)) (s z))
+    (z (s z) (s (s z)))
+    ((s z) (s (s z)) z)
+    ((s (s z)) (s z) z)
+    ((s z) z (s (s z)))
+    ((s (s z)) z (s z))))
